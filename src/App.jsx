d7f35353c602568f8cc868b289e4ac98de0f25a6
pathanyawarkhan785 <pathanyawarkhan785@gmail.com>
2022-05-23 11:59:44 +0530

    maping

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,16 @@ const App = () => {
 
             <h1 className='movieList'>Movies list</h1>
             <div className='list'>
-                <div className='each-movie'>
-                    <img src={siteData[0].thumbnail} alt='loading' width="250px" height="300px"></img>
-                    <h1 className='title'>{siteData[0].title}</h1>
-                    <a href={siteData[0].watchLink} target="_blank" rel="noreferrer" className='link spaceMargin'>{watch}</a>
-                    <div className='netflix spaceMargin'>{netflix}</div>
-                </div>
-
-
-                <div className='each-movie'>
-                    <img src={siteData[1].thumbnail} alt='loading' width="250px" height="300px"></img>
-                    <h1 className='title'>{siteData[1].title}</h1>
-                    <a href={siteData[1].watchLink} target="_blank" rel="noreferrer" className='link spaceMargin'>{watch}</a>
-                    <div className='netflix spaceMargin'>{netflix}</div>
-                </div>
-
-
-                <div className='each-movie'>
-                    <img src={siteData[2].thumbnail} alt='loading' width="250px" height="300px"></img>
-                    <h1 className='title'>{siteData[2].title}</h1>
-                    <a href={siteData[2].watchLink} target="_blank" rel="noreferrer" className='link spaceMargin'>{watch}</a>
-                    <div className='netflix spaceMargin'>{netflix}</div>
-                </div>
+                {siteData.map((movie) =>
+                    <div className='each-movie'>
+                        <img src={movie.thumbnail} alt='loading' width="250px" height="300px"></img>
+                        <h1 className='title'>{movie.title}</h1>
+                        <a href={movie.watchLink} target="_blank" rel="noreferrer" className='link spaceMargin'>{watch}</a>
+                        <div className='netflix spaceMargin'>{netflix}</div>
+                    </div>
+                )}
             </div>
         </>
-
     )
 }
 
